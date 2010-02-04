1df28286a76d7e038d6f5c737b6fa9cbb86492bc
Cyril Mougel <cyril.mougel@af83.com>
2010-02-04 11:52:44 +0100

    delete some useless comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,4 @@ class User
   ## Others available are :lockable, :timeoutable and :activatable.
   devise :authenticatable, :confirmable, :recoverable, :rememberable, :trackable, :validatable
 
-  ## Setup accessible (or protected) attributes for your model
-  ##attr_accessible :email, :password, :password_confirmation
-
-  #key :email, String
-
 end
