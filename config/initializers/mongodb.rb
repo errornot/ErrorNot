de4037b2e9c693425929bfafdfe45c5dc7588690
Cyril Mougel <cyril@yeastymobs.com>
2010-03-26 18:04:42 +0100

    use latest connection system in MongoMapper and allow connection by auth

--- config/initializers/mongodb.rb
+++ config/initializers/mongodb.rb
@@ -2,8 +2,5 @@ db_config = YAML::load(File.read(RAILS_ROOT + "/config/database.yml"))
 
 if db_config[Rails.env] && db_config[Rails.env]['adapter'] == 'mongodb'
   mongo = db_config[Rails.env]
-  MongoMapper.connection = Mongo::Connection.new(mongo['hostname'],
-                                                 mongo['port'] || 27017,
-                                                :logger => Rails.logger)
-  MongoMapper.database = mongo['database']
+  MongoMapper.setup(db_config, Rails.env, :logger => Rails.logger)
 end
