8526a91fd9eca7ca0eb07d5bae89c808de3cc6cb
Cyril Mougel <cyril.mougel@gmail.com>
2010-03-31 00:41:04 +0200

    create error_embedded in migrate. Forget previous

--- db/migrate/001_extract_same_error.rb
+++ db/migrate/001_extract_same_error.rb
@@ -1,6 +1,12 @@
 puts 'extract error_embedded from error collection'
 Error.collection.find.each do |er|
   if er['same_errors']
+    ErrorEmbedded.create!(:error_id => er['_id'],
+                         :session => er['session'],
+                         :raised_at => er['raised_at'],
+                         :request => er['request'],
+                         :environment => er['environment'],
+                         :data => er['data'])
     er.delete('same_errors')
     Error.collection.update({'_id' => er['_id']}, er)
   end
