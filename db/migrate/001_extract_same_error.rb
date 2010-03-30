8b4d4db3731db18b06e4340bd47b2fbed6996595
Cyril Mougel <cyril.mougel@gmail.com>
2010-03-31 01:01:39 +0200

    fix error on migration script because delete old before create new same_error

--- db/migrate/001_extract_same_error.rb
+++ db/migrate/001_extract_same_error.rb
@@ -1,13 +1,17 @@
 puts 'extract error_embedded from error collection'
 Error.collection.find.each do |er|
   if er['same_errors']
-    ErrorEmbedded.create!(:error_id => er['_id'],
-                         :session => er['session'],
-                         :raised_at => er['raised_at'],
-                         :request => er['request'],
-                         :environment => er['environment'],
-                         :data => er['data'])
+    errors = []
+    er['same_errors'].each do |same_error|
+      errors << ErrorEmbedded.new(:error_id => er['_id'],
+                           :session => same_error['session'],
+                           :raised_at => same_error['raised_at'],
+                           :request => same_error['request'],
+                           :environment => same_error['environment'],
+                           :data => same_error['data'])
+    end
     er.delete('same_errors')
     Error.collection.update({'_id' => er['_id']}, er)
+    errors.map(&:save!)
   end
 end
